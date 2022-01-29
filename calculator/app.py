e357a1885beb1c57a3b7ef09c61f5b948ae51e58
Azizbek Developer <azizbekqozoqovinfo@gmail.com>
2022-01-29 16:17:39 +0500

    Deploying Calculator v1.2.0

--- calculator/app.py
+++ calculator/app.py
@@ -1,3 +1,4 @@
+import sys
 from tkinter import *
 from tkinter.messagebox import showinfo, showwarning, showerror, askquestion,askokcancel
 
@@ -6,61 +7,88 @@ root = Tk()
 root.title("Calculator app")
 root.minsize(310, 440)
 root.maxsize(40, 30)
+if sys.platform == "linux":
+    root.tk.call('wm', 'iconphoto', root._w, PhotoImage(file='/home/developer/Documents/projects/python_mini_projects/calculator/icon.png'))
+
+
 # input
 e = Entry(root, width=35, borderwidth=3)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
+
 def button_add(number):
-    current = e.get()
-    e.delete(0, END)
-    e.insert(0, current+ str(number))
+    try:
+        current = e.get()
+        e.delete(0, END)
+        e.insert(0, current+ str(number))
+    except:
+        showwarning("Warn", "Please enter right expression!")
 
 def clear():
     e.delete(0, END)
+
 def add():
-    first_number = e.get()
-    global f_num
-    global math
-    math = "addition"
-    f_num = int(first_number)
-    e.delete(0, END)
+    try:
+
+        first_number = e.get()
+        global f_num
+        global math
+        math = "addition"
+        f_num = int(first_number)
+        e.delete(0, END)
+    except:
+        showwarning("Warn", "Please enter right expression!")
 
 def equal():
-    second_number = e.get()
-    e.delete(0, END)
+    try:
+        second_number = e.get()
+        e.delete(0, END)
 
-    if math == "addition":
-        e.insert(0, f_num + int(second_number))
-    if math == "subtracktion":
-        e.insert(0, f_num - int(second_number))
-    if math == "multiplication":
-        e.insert(0, f_num * int(second_number))
-    if math == "divison":
-        e.insert(0, f_num / int(second_number))
+        if math == "addition":
+            e.insert(0, f_num + int(second_number))
+        if math == "subtracktion":
+            e.insert(0, f_num - int(second_number))
+        if math == "multiplication":
+            e.insert(0, f_num * int(second_number))
+        if math == "divison":
+            e.insert(0, f_num / int(second_number))
+    except:
+        showwarning("Warn", "Please enter right expression!")
 
 def subtrackt():
-    first_number = e.get()
-    global f_num
-    global math
-    math = "subtracktion"
-    f_num = int(first_number)
-    e.delete(0, END)
+    try:
+
+        first_number = e.get()
+        global f_num
+        global math
+        math = "subtracktion"
+        f_num = int(first_number)
+        e.delete(0, END)
+    except:
+        showwarning("Warn", "Please enter right expression!")
 
 def multiply():
-    first_number = e.get()
-    global f_num
-    global math
-    math = "multiplication"
-    f_num = int(first_number)
-    e.delete(0, END)
+    try:
+        first_number = e.get()
+        global f_num
+        global math
+        math = "multiplication"
+        f_num = int(first_number)
+        e.delete(0, END)
+    except:
+        showwarning("Warn", "Please enter right expression!")
 
 def divide():
-    first_number = e.get()
-    global f_num
-    global math
-    math = "divison"
-    f_num = int(first_number)
-    e.delete(0, END)
+    try:
+
+        first_number = e.get()
+        global f_num
+        global math
+        math = "divison"
+        f_num = int(first_number)
+        e.delete(0, END)
+    except:
+        showwarning("Warn", "Please enter right expression!")
 
 button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_add(1))
 button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_add(2))
@@ -101,4 +129,6 @@ button_subtrackt.grid(row=6, column=0)
 button_multiply.grid(row=6, column=1)
 button_divide.grid(row=6, column=2)
 
+
+# loop
 root.mainloop()
